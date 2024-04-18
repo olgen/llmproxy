262d9b30fa66aa6cad833882082291b6be3e3d01
Eugen Martin <eugeniusmartinus@gmail.com>
2024-04-18 14:35:23 +0200

    setup simple proxy

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const llmProxy = express();
+// Configuration
+const port = process.env.PORT;
+const llmUrl = "https://api.openai.com";
+
+llmProxy.use(
+  "/",
+  createProxyMiddleware({
+    target: llmUrl,
+    changeOrigin: true,
+    headers: {
+      Authorization: "Bearer " + process.env.OPENAI_API_KEY,
+    },
+  })
+);
+
+llmProxy.listen(port, () => {
+  console.log(`Started Proxy at:${port}`);
+});
